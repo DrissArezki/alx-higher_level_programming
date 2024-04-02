25ac913aa530d19e781dfc25be0f9ba0c8c8143d
DrissArezki <driss.arezki5@gmail.com>
2024-04-02 07:31:05 -0700

    0x05-python-exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             ctr += 1
         except (ValueError, TypeError):
             pass
-        print()
-        return ctr
+    print()
+    return ctr
