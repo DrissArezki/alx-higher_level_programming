d477d7eff7dcd6305d7b21cb9ab4d29da22b7c6b
DrissArezki <driss.arezki5@gmail.com>
2024-04-03 09:38:29 -0700

    classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
 
 
         Arguments are:
-        size (int): is the size of the new square.
+            size (int): is the size of the new square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
