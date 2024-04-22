42962cb608688cc2eec68ac0d6d36f366300c6d9
DrissArezki <driss.arezki5@gmail.com>
2024-04-22 16:56:51 -0700

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -70,11 +70,12 @@ class Base:
         from models.square import Square
         if list_objs is not None:
             if cls is Rectangle:
-                list_objs = [[o.id, o.width, o.height, o.x, o.y] for o in list_objs]
+                list_objs = [[o.id, o.width, o.height, o.x, o.y]
+                             for o in list_objs]
             else:
                 list_objs = [[o.id, o.size, o.x, o.y] for o in list_objs]
         with open('{}.csv'.format(cls.__name__), 'w', newline=' ',
-                 encoding='utf-8') as f:
+                  encoding='utf-8') as f:
             writer = csv.writer(f)
             writer.writerows(list_objs)
 
@@ -85,12 +86,12 @@ class Base:
         from models.square import Square
         tier = []
         with open('{}.csv'.format(cls.__name__), 'r', newline=' ',
-                 encoding='utf-8') as f:
+                  encoding='utf-8') as f:
             reader = csv.reader(f)
             for row in reader:
                 row = [int(r) for r in row]
                 if cls is Rectangle:
-                    d = {"id": row[0], "width": row[1], "height": row[2], 
+                    d = {"id": row[0], "width": row[1], "height": row[2],
                          "x": row[3], "y": row[4]}
                 else:
                     d = {"id": row[0], "size": row[1],
