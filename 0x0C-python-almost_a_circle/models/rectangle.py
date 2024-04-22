77d0f4526964b81894a06381bdf9b231a7485fce
DrissArezki <driss.arezki5@gmail.com>
2024-04-22 15:34:43 -0700

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -41,7 +41,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        self.validate_integer("x", value)
+        self.validate_integer("x", value, False)
         self.__x = value
 
     @property
@@ -51,7 +51,7 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        self.validate_integer("y", value)
+        self.validate_integer("y", value, False)
         self.__y = value
 
     def validate_integer(self, name, value, positive=True):
