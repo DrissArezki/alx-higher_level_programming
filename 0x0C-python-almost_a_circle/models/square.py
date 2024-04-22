cbec461bd375dd6be3cb39033958021e6b3fe21a
DrissArezki <driss.arezki5@gmail.com>
2024-04-22 16:01:27 -0700

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,10 +8,10 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         '''Constructor.'''
-        super().__init__(size, size, x, y, id)  # corrected size argument
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         '''Returns the info of the square.'''
         return '[{}] ({}) {}/{} - {}'.\
-            format(type(self).__name__, self.id, self.x, self.y, self.width)  # corrected typo "slef" to "self"
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
 
